98fb3ca9e9c6d0b33a2a66f4964bb82f4b6a281b
Siddharth Srinivasan <siddharths2710phone@gmail.com>
2017-11-22 08:38:18 +0530

    Added connection to mongoDB server

--- RecipeApp/routes/index.js
+++ RecipeApp/routes/index.js
@@ -1,11 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var fs = require('fs');
+var mongoose = require('mongoose');
+
 /* GET home page. */
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
+var Schema = mongoose.Schema;
+
+var recipeSchema = new Schema({
+
+title:String,
+author:String,
+imgurl:String,
+ingredients:Array,
+procedure:String,
+likes:Number,
+Comments: Array
+
+});
+
+var mongoDB = "mongodb://crazyboy:incorrect@ds245715.mlab.com:45715/crowdsource_recipes";
+
+mongoose.connect(mongoDB);
+
+var recipes = mongoose.model('recipe',recipeSchema);
+
 router.get('/', function(req, res, next)
 {
-  var obj;
+  /*var obj;
   fs.readFile(path.join(__dirname,"../","public","data","testfile.json"),
     function(err,jsFile,obj)
     {
@@ -20,7 +46,14 @@ router.get('/', function(req, res, next)
           res.render('myview', obj);
         }
     }
-  )
+  )*/
+
+  recipes.find({},function(err,jsonResp){
+
+        //jsonResp[0].procedure = jsonResp[0].procedure.replaceAll('\r\n','<br><br>').replaceAll('\n','<br><br>');
+        res.render('myview',{recipes: jsonResp});
+
+  });
 
 });
 
